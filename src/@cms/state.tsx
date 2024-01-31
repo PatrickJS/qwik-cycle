218f8923bfa1a397f558a4bf497e222809493fb4
PatrickJS <github@patrickjs.com>
2024-01-31 15:03:56 -0800

    wip

--- src/@cms/state.tsx
+++ src/@cms/state.tsx
@@ -12,10 +12,17 @@ export const SERVER_STATE = "SERVER_STATE";
 
 export const ServerContext = createContextId<Signal<any>>("server.context");
 
+export const EditContext = createContextId<Signal<any>>("server.edit");
+export const EditFieldContext = createContextId<Signal<any>>("server.field");
+
 export const ServerProvider = component$(() => {
   const serverData = useServerData(SERVER_STATE, {});
   const server = useSignal(serverData);
+  const edit = useSignal(false);
+  const editField = useSignal("");
   useContextProvider(ServerContext, server);
+  useContextProvider(EditContext, edit);
+  useContextProvider(EditFieldContext, editField);
 
   return <Slot />;
 });
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,76 +5,102 @@ import {
   $,
   useSignal,
   // Slot,
-  useServerData,
+  // useServerData,
   useTask$,
-  Signal,
+  // Signal,
   useContext,
-  useContextProvider,
   Slot,
-  HTMLAttributes,
+  // useContextProvider,
+  // Slot,
+  // HTMLAttributes,
 } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
-import { ServerContext } from "~/@cms/state";
+import { EditContext, EditFieldContext, ServerContext } from "~/@cms/state";
+// import { EditField } from "~/@cms/state";
 
 export const EDIT_EVENT = "app_edit";
 export const CONFIRM_EVENT = "app_confirm";
 
 export interface Props {
-  bind: Signal<boolean>;
   path: any;
   content: any;
 }
 export const EditText = component$((props: Props) => {
   const serverContext = useContext(ServerContext);
-  // const serverData = useServerData(SERVER_STATE, props.content);
-
-  // undefined on client after initial load even as data is transferred
-  // const serverContent = serverData[props.path];
-
-  const editMode = useSignal(false);
-  // const serverDataSignal = useSignal(serverContext.value[props.path]);
-
-  // useOnDocument(
-  //   EDIT_EVENT,
-  //   $((evt: any) => {
-  //     console.log(EDIT_EVENT, evt.detail, serverDataSignal.value);
-  //     if (evt.detail.type === "edit") {
-  //       editMode.value = true;
-  //     } else if (evt.detail.type === "confirm") {
-  //       editMode.value = false;
-  //     }
-  //     editMode.value = editMode.value;
-  //   }),
-  // );
-
+  const edit = useContext(EditContext);
+  const editField = useContext(EditFieldContext);
+  const ref = useSignal<HTMLSpanElement>();
+  const newContent = useSignal("");
   useTask$(({ track }) => {
-    track(editMode);
-    // track(serverDataSignal);
-    track(props.bind);
-    editMode.value = props.bind.value;
-    console.log(
-      "task editMode.value",
-      editMode.value,
-      serverContext.value[props.path],
-    );
+    track(edit);
+    console.log("edit.value", edit.value);
   });
-  // useOnDocument(
-  //   CONFIRM_EVENT,
-  //   $((evt: any) => {
-  //     console.log(CONFIRM_EVENT, evt);
-  //     editMode.value = false;
-  //     console.log("editMode.value", editMode.value, serverDataSignal.value);
-  //   }),
-  // );
 
-  if (editMode.value === true) {
+  // const editMode = useSignal(editField.value === props.path && edit.value);
+  useTask$(({ track }) => {
+    track(ref);
+    if (edit.value && ref.value) {
+      const el = ref.value;
+      el.focus();
+
+      function inputLastRange(element: HTMLElement) {
+        //Create a range (a range is a like the selection but invisible)
+        const range = document.createRange();
+        //Select the entire contents of the element with the range
+        range.selectNodeContents(element);
+        //collapse the range to the end point. false means collapse to end rather than the start
+        range.collapse(false);
+        //get the selection object (allows you to change selection)
+
+        const selection = window.getSelection(); //get the selection object (allows you to change selection)
+        if (selection) {
+          selection.removeAllRanges(); //remove any selections already made
+          selection.addRange(range); //make the range you have just created the visible selection
+        }
+      }
+      inputLastRange(el);
+    }
+    //   // track(editMode);
+    //   // track(serverDataSignal);
+    //   track(() => editField.value);
+    //   if (editField.value !== props.path) {
+    //     return;
+    //   }
+    //   // editMode.value = edit.value;
+  });
+  // useTask$(({ track }) => {
+  //   track(edit);
+  //   if (edit.value === false && ref.value) {
+  //     newContent.value = ref.value.textContent || "";
+  //     return;
+  //   }
+  // });
+  // console.log(
+  //   "editMode.value",
+  //   // editMode.value,
+  //   edit.value,
+  //   editField.value,
+  //   props.path,
+  //   editField.value === props.path,
+  // );
+  console.log("newContent", newContent.value);
+  if (newContent.value && edit.value === false) {
+    return newContent.value;
+  }
+
+  if (edit.value === true && editField.value === props.path) {
+    console.log("EDIT MODE");
     return (
       <span
+        ref={ref}
         // @ts-ignore
-        contentEditable={editMode.value ? "plaintext-only" : "false"}
-        onInput$={(evt, target) => {
-          console.log("input enter", target?.textContent);
-        }}
+        contentEditable={"plaintext-only"}
+        // onInput$={(evt) => {
+        //   debugger;
+        //   newContent.value = `${evt.data}`;
+        //   // debugger;
+        //   console.log("input enter", newContent.value);
+        // }}
         onKeyDown$={(evt: any) => {
           console.log(
             "key down",
@@ -85,62 +111,86 @@ export const EditText = component$((props: Props) => {
             evt,
           );
         }}
+        autofocus
       >
-        {props.content}
+        {newContent.value || props.content}
       </span>
     );
-  } else if (editMode.value === false && serverContext.value[props.path]) {
-    return serverContext.value[props.path];
+  } else if (serverContext.value[props.path]) {
+    // overwrite the content with the server value
+    return <div>?? {serverContext.value[props.path]}</div>;
   }
+  // default value by developer
   return props.content;
 });
 
 interface TextProps {
-  bind: Signal<boolean>;
   path: string;
-  children: any;
+  children?: any;
 }
-export function Text({ bind, path, children }: TextProps) {
-  return <EditText bind={bind} path={path} content={children} />;
-}
+export const Text = component$(function Text({ path }: TextProps) {
+  return <EditText path={path} content={<Slot />} />;
+});
 
 export default component$(() => {
-  const editMode = useSignal(false);
+  // const editMode = useSignal(false);
+  // const canEdit = useSignal(false);
+  const edit = useContext(EditContext);
+  const editField = useContext(EditFieldContext);
   useOnDocument(
     EDIT_EVENT,
     $((evt: any) => {
-      editMode.value = evt.detail;
-      console.log("editMode.value", editMode.value, evt.detail);
+      edit.value = evt.detail;
+      console.log("editMode.value", evt.detail);
     }),
   );
   return (
     <div class="m-5">
-      <div>
-        <button
-          onClick$={() => {
-            const detail = true;
-            document.dispatchEvent(new CustomEvent(EDIT_EVENT, { detail }));
-            console.log("clicked", EDIT_EVENT, detail);
-          }}
-        >
-          Edit
-        </button>
-        <button
-          onClick$={() => {
-            const detail = false;
-            document.dispatchEvent(new CustomEvent(EDIT_EVENT, { detail }));
-            console.log("clicked", EDIT_EVENT, detail);
-          }}
-        >
-          Confirm
-        </button>
-      </div>
+      {edit.value && (
+        <div>
+          <button
+            onClick$={() => {
+              // const detail = true;
+              // document.dispatchEvent(new CustomEvent(EDIT_EVENT, { detail }));
+              // console.log("clicked", EDIT_EVENT, detail);
+              if (editField.value === "appText") {
+                editField.value = "";
+                return;
+              } else {
+                editField.value = "appText";
+              }
+              console.log("editField.value", editField.value);
+            }}
+          >
+            Edit {editField.value === "appText" ? "Done" : "'appText'"}
+          </button>
+          <button
+            onClick$={() => {
+              const detail = false;
+              edit.value = detail;
+              // document.dispatchEvent(new CustomEvent(EDIT_EVENT, { detail }));
+              console.log("clicked", EDIT_EVENT, detail);
+            }}
+          >
+            Confirm
+          </button>
+        </div>
+      )}
+
       <div class="flex flex-col">
-        <div>lol</div>
-        <Text bind={editMode} path="appText">
-          Loading... hello world
-        </Text>
+        <h3>Welcome</h3>
+        <div>
+          <Text path="appText">Default View</Text>
+        </div>
       </div>
+      {edit.value === false && (
+        <div class="fixed bottom-0 right-0 m-4 p-4 text-white">
+          {/* @ts-ignore */}
+          <button onClick="document.dispatchEvent(new CustomEvent('app_edit', { detail: true }));">
+            Edit Page
+          </button>
+        </div>
+      )}
     </div>
   );
 });
